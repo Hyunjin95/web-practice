4972fcde2f69cd5f7b63b9952a35d52947a18f55
Hyunjin95 <das01063@snu.ac.kr>
2019-10-09 21:38:09 +1000

    Load with AsyncComponent

--- code_splitting/src/App.js
+++ code_splitting/src/App.js
@@ -3,36 +3,37 @@ import './App.css';
 import Page1 from './components/Page1';
 // import Page2 from './components/Page2';
 // import Page3 from './components/Page3';
+import AsyncComponent from './components/AsyncComponent';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
-      component: Page1
+      component: null
     }
   }
 
   onRouteChange = (route) => {
     // No code splitting
-    // this.setState({route});
+    this.setState({route});
 
-    // With code splitting
-    if (route === 'page1') {
-      this.setState({ route, component: Page1 });
-    }
-    else if (route === 'page2') {
-      import('./components/Page2')
-        .then((Page2) => {
-          this.setState({ route, component: Page2.default });
-        });
-    }
-    else if (route === 'page3') {
-      import('./components/Page3')
-        .then((Page3) => {
-          this.setState({ route, component: Page3.default });
-        });
-    }
+    // With code splitting - Step 1
+    // if (route === 'page1') {
+    //   this.setState({ route });
+    // }
+    // else if (route === 'page2') {
+    //   import('./components/Page2')
+    //     .then((Page2) => {
+    //       this.setState({ route, component: Page2.default });
+    //     });
+    // }
+    // else if (route === 'page3') {
+    //   import('./components/Page3')
+    //     .then((Page3) => {
+    //       this.setState({ route, component: Page3.default });
+    //     });
+    // }
   }
 
   render() {
@@ -56,7 +57,28 @@ class App extends React.Component {
     //   </div>
     // );
 
-    return <this.state.component onRouteChange={this.onRouteChange} />;
+    // Code splitting - Step 1
+    // if (route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />;
+    // }
+    // else {
+    //   return <this.state.component onRouteChange={this.onRouteChange} />;
+    // }
+
+    // Code Splitting - Step 2
+    // The downside of this method is that a browser flashes when it first loads each page.
+    // Because JS modules need to be imported. Also, the file sizes could sometimes be bigger.
+    if (route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />;
+    }
+    else if (route === 'page2') {
+      const AsyncPage2 = AsyncComponent(() => import('./components/Page2'));
+      return <AsyncPage2 onRouteChange={this.onRouteChange} />;
+    }
+    else if (route === 'page3') {
+      const AsyncPage3 = AsyncComponent(() => import('./components/Page3'));
+      return <AsyncPage3 onRouteChange={this.onRouteChange} />;
+    }
   }
 }
 
