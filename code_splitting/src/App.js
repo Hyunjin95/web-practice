2f02481e148c0e0a495cfa56aeaaefbfbcdb5624
Hyunjin95 <das01063@snu.ac.kr>
2019-10-09 20:19:20 +1000

    Route the pages

--- code_splitting/src/App.js
+++ code_splitting/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
@@ -18,25 +17,21 @@ class App extends React.Component {
   }
 
   render() {
+    const { route } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <Page1 onRouteChange={this.onRouteChange} />
-        <Page2 onRouteChange={this.onRouteChange} />
-        <Page3 onRouteChange={this.onRouteChange} />
+      <div>
+        {route === 'page1' ? (
+          <Page1 onRouteChange={this.onRouteChange} />
+        )
+        : route === 'page2' ? (
+          <Page2 onRouteChange={this.onRouteChange} />
+        )
+        : route === 'page3' ? (
+          <Page3 onRouteChange={this.onRouteChange} />
+        )
+        : (
+          <h1>Unforeseen error has occured!!</h1>
+        )}
       </div>
     );
   }
