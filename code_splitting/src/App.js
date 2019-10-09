d26147dd39edb44a8fbfcc287fd93af36348adb2
Hyunjin95 <das01063@snu.ac.kr>
2019-10-09 20:44:12 +1000

    Import the pages dynamically

--- code_splitting/src/App.js
+++ code_splitting/src/App.js
@@ -1,39 +1,62 @@
 import React from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Page3 from './components/Page3';
+// import Page2 from './components/Page2';
+// import Page3 from './components/Page3';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       route: 'page1',
+      component: Page1
     }
   }
 
   onRouteChange = (route) => {
-    this.setState({route});
+    // No code splitting
+    // this.setState({route});
+
+    // With code splitting
+    if (route === 'page1') {
+      this.setState({ route, component: Page1 });
+    }
+    else if (route === 'page2') {
+      import('./components/Page2')
+        .then((Page2) => {
+          this.setState({ route, component: Page2.default });
+        });
+    }
+    else if (route === 'page3') {
+      import('./components/Page3')
+        .then((Page3) => {
+          this.setState({ route, component: Page3.default });
+        });
+    }
   }
 
   render() {
     const { route } = this.state;
-    return (
-      <div>
-        {route === 'page1' ? (
-          <Page1 onRouteChange={this.onRouteChange} />
-        )
-        : route === 'page2' ? (
-          <Page2 onRouteChange={this.onRouteChange} />
-        )
-        : route === 'page3' ? (
-          <Page3 onRouteChange={this.onRouteChange} />
-        )
-        : (
-          <h1>Unforeseen error has occured!!</h1>
-        )}
-      </div>
-    );
+
+    // No code splitting
+    // return (
+    //   <div>
+    //     {route === 'page1' ? (
+    //       <Page1 onRouteChange={this.onRouteChange} />
+    //     )
+    //     : route === 'page2' ? (
+    //       <Page2 onRouteChange={this.onRouteChange} />
+    //     )
+    //     : route === 'page3' ? (
+    //       <Page3 onRouteChange={this.onRouteChange} />
+    //     )
+    //     : (
+    //       <h1>Unforeseen error has occured!!</h1>
+    //     )}
+    //   </div>
+    // );
+
+    return <this.state.component onRouteChange={this.onRouteChange} />;
   }
 }
 
