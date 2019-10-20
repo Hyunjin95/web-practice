95d860d5f29ec0553482cb6601fcdfe5239d2766
Hyunjin95 <das01063@snu.ac.kr>
2019-10-20 18:31:41 +1000

    Get my first test

--- test/script.test.js
+++ test/script.test.js
@@ -7,6 +7,22 @@ dbMock = [
     'dogpictures.com'
 ];
 
-it('this is a test', () => {
-    googleSearch('cat', dbMock);
+describe('googleSearch', () => {
+    it('just check if this works correctly', () => {
+        expect('hello').toBe('hello');
+    });
+    
+    it('basic tests', () => {
+        expect(googleSearch('cat', dbMock)).toEqual([]);
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+    });
+    
+    it('undefined and null inputs', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+    });
+    
+    it('not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+    });
 });
