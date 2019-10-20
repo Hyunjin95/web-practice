dfdf0c68edf1622e9460984435f6743d45971d64
Hyunjin95 <das01063@snu.ac.kr>
2019-10-20 19:46:13 +1000

    Make a mock

--- README.md
+++ README.md
@@ -21,6 +21,9 @@ The aim of the repository is to understand how to develop web applications.
 * Production/Deployment
   * Webpack
   * Parcel
+  * PWA
+* Performance
+  * Critical Rendering Path
 * Testing
   * Jest
 
--- test/script2.test.js
+++ test/script2.test.js
@@ -22,3 +22,24 @@ describe('getPeoplePromise', () => {
         });
     });
 });
+
+describe('Mocks and Spies', () => {
+    // Mirror the promises in order to reduce a testing time.
+    it('getPeopleAsync returns count and results', () => {
+        const mockFetch = jest.fn().
+            mockReturnValue(Promise.resolve({
+                json: () => Promise.resolve({
+                    count: 87,
+                    results: [0, 1, 2, 3, 4, 5]
+                })
+            }));
+
+        expect.assertions(4);
+        return swapi.getPeoplePromise(mockFetch).then(data => {
+            expect(mockFetch.mock.calls.length).toBe(1);
+            expect(mockFetch).toBeCalledWith('https://swapi.co/api/people');
+            expect(data.count).toEqual(87);
+            expect(data.results.length).toBeGreaterThan(5);
+        });
+    });
+});
