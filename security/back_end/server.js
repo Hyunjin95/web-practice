13778a24248bf66cec9ed0063c89fc8ac03d0390
Hyunjin95 <das01063@snu.ac.kr>
2019-10-31 20:51:37 +1000

    Set headers and cookies

--- security/back_end/server.js
+++ security/back_end/server.js
@@ -11,7 +11,14 @@ app.use(helmet());
 app.use(express.json());
 // app.use(morgan('combined'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+    res.cookie('session', '1', { httpOnly: true });
+    res.cookie('session', '1', { secure: true });
+    res.set({
+        'Content-Security-Policy': "script-src 'self' 'https://api.google.com'"
+    });
+    res.send('Hello World!');
+});
 
 app.post('/secret', (req, res) => {
     const {
