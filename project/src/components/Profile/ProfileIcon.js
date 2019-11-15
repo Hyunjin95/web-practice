4585c6263f9734eeb98a5429adc6c4ddc8606d88
Hyunjin95 <das01063@snu.ac.kr>
2019-11-15 21:21:01 +1000

    Add a dropdown feature

--- project/src/components/Profile/ProfileIcon.js
+++ project/src/components/Profile/ProfileIcon.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 
 class ProfileIcon extends React.Component {
@@ -9,10 +10,24 @@ class ProfileIcon extends React.Component {
         };
     }
 
+    toggle = () => {
+        this.setState(prevState => ({
+            dropdownOpen: !prevState.dropdownOpen
+        }));
+    }
+
     render() {
         return (
-            <div class="pa4 tc">
-                <img src="http://tachyons.io/img/logo.jpg" class="br-100 ba h3 w3 dib" alt="avatar" />
+            <div className='pa4 tc'>
+                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle} style={{ cursor: 'pointer' }}>
+                    <DropdownToggle tag="span" data-toggle="dropdown" aria-expanded={this.state.dropdownOpen}>
+                        <img src="http://tachyons.io/img/logo.jpg" className="br-100 ba h3 w3 dib" alt="avatar" />
+                    </DropdownToggle>
+                    <DropdownMenu className="b--transparent shadow-5" style={{ backgroundColor: '(rgba(255, 255, 255, 0.5' }}>
+                        <DropdownItem>View Profile</DropdownItem>
+                        <DropdownItem>Signout</DropdownItem>
+                    </DropdownMenu>
+                </Dropdown>
             </div>
         );
     }
