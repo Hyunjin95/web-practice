a3fbd8baac96e949e6881e645c3f0877cdf3badf
Hyunjin95 <das01063@snu.ac.kr>
2019-11-24 21:00:21 +1000

    Make the app use an AWS lambda function

--- project/src/components/Rank/Rank.js
+++ project/src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
 import React from 'react';
 
-const Rank = ({ name, entries }) => {
-    return (
-        <div>
-            <div className='white f3'>
-                {`${name}, your current count number is ...`}
+class Rank extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emoji: ''
+        }
+    }
+
+    componentDidMount() {
+        this.getEmoji(this.props.entries);
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.entries === this.props.entries && prevProps.name === this.props.name) {
+            return null;
+        }
+        this.getEmoji(this.props.entries);
+    }
+
+    getEmoji = (entries) => {
+        fetch(`https://5jxq4vx8x9.execute-api.us-east-1.amazonaws.com/dev/rank?rank=${entries}`)
+            .then(res => res.json())
+            .then(data => this.setState({ emoji: data.input }))
+            .catch(err => console.log(err));
+    }
+
+    render() {
+        const { name, entries } = this.props;
+        return (
+            <div>
+                <div className='white f3'>
+                    {`${name}, your current count number is ...`}
+                </div>
+                <div className='white f1'>
+                    {entries}
+                </div>
+                <div className='white f3'>
+                    {`Your Rank Badge: ${this.state.emoji}`}
+                </div>
             </div>
-            <div className='white f1'>
-                {entries}
-            </div>
-        </div>
-    )
-};
+        )
+    }
+}
 
 export default Rank;
--- serverless/handler.js
+++ serverless/handler.js
@@ -5,9 +5,12 @@ const emojis = ["✌","😂","😝","😁","😱","👉","🙌","🍻","🔥","�
 
 module.exports.rank = async event => {
   const rank = event.queryStringParameters.rank;
-  const rankEmoji = emojis[rank > emojis.length ? emojis.length - 1 : rank];
+  const rankEmoji = emojis[rank > emojis.length-1 ? emojis.length-1 : rank];
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
