5d9e0c43baa567f38b0b44b1f60564a9878cdfe4
Hyunjin95 <das01063@snu.ac.kr>
2020-01-29 02:52:46 +0900

    Change the cursor to pointer

--- react/robotfriends-nextjs/components/Card.tsx
+++ react/robotfriends-nextjs/components/Card.tsx
@@ -6,7 +6,7 @@ const Card: React.FC<Robot> = (props: Robot) => {
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <Link href={`/robots?id=${id}`} as={`robots/${id}`}>
-        <button className="no-underline" type="button">
+        <button style={{cursor: 'pointer'}} className="no-underline" type="button">
           <img alt="_robots_" src={`https://robohash.org/${id}?size=200x200`} />
           <div>
             <h2>{name}</h2>
--- react/robotfriends-nextjs/pages/robots.tsx
+++ react/robotfriends-nextjs/pages/robots.tsx
@@ -24,7 +24,7 @@ const Robots: StatelessPage<RobotProps> = ({
   isError = false,
   robot,
 }: RobotProps) => {
-  const pf = prefix !== '' ? '/web-practice' : '/';
+  const pf = process.env.NODE_ENV === 'production' ? '/web-practice' : "/";
 
   return (
     <Layout prefix={prefix}>
