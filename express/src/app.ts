600fd2814c38689dcfb58be8cc2f024919ef2de4
hyunjinjeong <hyunjin.dev@gmail.com>
2020-04-14 14:55:51 +0900

    Set up the main page
    - There are small changes in .prettierrc.json and .eslintrc.json.
    - nodemon has been applied.
    - Add the main page.

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, '../', 'public/main.html'));
 });
 
 app.listen(3000, () => {
